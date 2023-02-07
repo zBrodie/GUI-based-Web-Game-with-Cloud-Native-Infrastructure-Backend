9f1b62c0cb9b863287529b919adc32d5de0ef3eb
Aaron Cole <themahimusic@gmail.com>
2023-02-07 02:40:13 -0500

    Add files via upload

--- Game.js
+++ Game.js
@@ -1,27 +1,39 @@
-export const UpwardsMobility = {
-    setup: () => ({ track1: Array(10).fill(null)}
-            // ,
-        // {track2: Array(10).fill(null)},
-        // {track3: Array(10).fill(null)}
-    ),
+import {TurnOrder} from "boardgame.io/core";
 
-    turn: {
-        minMoves: 1,
-        maxMoves: 1,
-    },
+
+export const UpwardsMobility = {
+    setup: () => ( { cells: Array(50).fill(null),
+        // player1Position: 0,
+        // plsyer2Position: 0
+        players: {
+            "0" : {position: 0},
+            "1" : {position: 0}
+        }
+    } ),
 
     moves: {
-    moveCell: ({ G, playerID }, id) => {
-        G.cells[id] = playerID;
+        moveCell: ({G, ctx}, moveDist, id) => {
+            console.log(G)
+            let die1 = Math.ceil(Math.random() * 6)
+            let die2 = Math.ceil(Math.random() * 6)
+            moveDist = die1 + die2
+            console.log(moveDist)
+            // if (ctx.currentPlayer === "0") {
+            //     G.player1Position += moveDist;
+            //     G.cells[G.player1Position] = id
+            // } else {
+            //     G.player2Position += moveDist;
+            //     G.cells[G.player2Position] = id
+            // }
+            G.players[ctx.currentPlayer].position += moveDist
+
+            G.cells[G.players[ctx.currentPlayer].position] = id
         },
     },
 
+    turn: {
+        order: TurnOrder.ONCE,
+        minMoves: 1,
+        maxMoves: 1,
+    },
 };
-
-function IsVictory(playerPosition) {
-    if (playerPosition == true) {
-        return true;
-    }
-    else
-        return false;
-}
