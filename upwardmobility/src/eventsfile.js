e1bac189ee18fd80f7d646816bd1ce414d0fcdf2
rocco <roccozeno@gmail.com>
2023-03-27 23:07:21 -0400

    written events to eventsfile.js

--- upwardmobility/src/eventsfile.js
+++ upwardmobility/src/eventsfile.js
@@ -2,8 +2,12 @@ import { hideRollScreen, showRollScreen} from "./Game";
 export function getEvent(event, G, ctx, events) {
 
     let eventScreen = document.getElementById("eventScreen");
-
-
+    const container = document.createElement("div");
+    const btnContainer = document.createElement("div");
+    let eventText = document.createElement("span");
+    let ans1 = document.createElement("button");
+    let ans2 = document.createElement("button");
+    let ans3 = document.createElement("button");
     switch (event) {
         case "wizardEvent":
             console.log("switch case function wizardEvent");
@@ -11,7 +15,7 @@ export function getEvent(event, G, ctx, events) {
             hideRollScreen();
 
             // Create container div element
-            const container = document.createElement("div");
+            //const container = document.createElement("div");
             container.setAttribute("class", "eventContainer");
             container.style.display = "flex";
             container.style.flexDirection = "column";
@@ -21,7 +25,7 @@ export function getEvent(event, G, ctx, events) {
             container.style.bottom = "40%"; // move container to bottom
             container.style.position = "absolute";
 
-            const btnContainer = document.createElement("div");
+            //const btnContainer = document.createElement("div");
             btnContainer.setAttribute("class", "eventButtonContainer");
             btnContainer.style.display = "flex";
             btnContainer.style.justifyContent = "center";
@@ -32,7 +36,7 @@ export function getEvent(event, G, ctx, events) {
             btnContainer.style.bottom = "0"; // move buttons down
             btnContainer.style.width = "100%";
 
-            let eventText = document.createElement("span");
+            //let eventText = document.createElement("span");
             eventText.innerHTML = "A mysterious raggedy wizard appears before you and asks the question... \"What is the airspeed velocity of an unladen swallow?\"";
             eventText.setAttribute("class", "inGameText");
             eventText.style.width = "60%";
@@ -45,9 +49,9 @@ export function getEvent(event, G, ctx, events) {
             // eventImg.id = "montyPython";
             // eventImg.src = "explosion.jpeg";
 
-            let ans1 = document.createElement("button");
-            let ans2 = document.createElement("button");
-            let ans3 = document.createElement("button");
+            //let ans1 = document.createElement("button");
+            //let ans2 = document.createElement("button");
+           // let ans3 = document.createElement("button");
 
             ans1.setAttribute("class", "answerButton");
             ans2.setAttribute("class", "answerButton");
@@ -90,6 +94,100 @@ export function getEvent(event, G, ctx, events) {
                     pickUpItem.style.visibility = "hidden";
 
                     eventText.innerHTML = "You pick up the \"Staff of NoMoney\" and feel a surge of power and greed!";
+
+                    btnContainer.removeChild(pickUpItem);
+                    let endTurnBtn = document.getElementById("GameEndTurn");
+                    btnContainer.appendChild(endTurnBtn);
+
+                    endTurnBtn.style.visibility = "visible";
+
+                });
+            });
+            break;
+
+        case "EventTemplateMC":
+            console.log("Load EVent template");
+
+            hideRollScreen();
+
+            // Create container div element
+            //const container = document.createElement("div");
+            container.setAttribute("class", "eventContainer");
+            container.style.display = "flex";
+            container.style.flexDirection = "column";
+            container.style.alignItems = "center";
+            container.style.height = "50%"; // reduce height
+            container.style.width = "100%";
+            container.style.bottom = "40%"; // move container to bottom
+            container.style.position = "absolute";
+
+            //const btnContainer = document.createElement("div");
+            btnContainer.setAttribute("class", "eventButtonContainer");
+            btnContainer.style.display = "flex";
+            btnContainer.style.justifyContent = "center";
+            btnContainer.style.alignItems = "center";
+            btnContainer.style.flexDirection = "row";
+            btnContainer.style.gap = "10px";
+            btnContainer.style.position = "absolute";
+            btnContainer.style.bottom = "0"; // move buttons down
+            btnContainer.style.width = "100%";
+
+            //let eventText = document.createElement("span");
+            eventText.innerHTML = "Event Text Goes here";
+            eventText.setAttribute("class", "inGameText");
+            eventText.style.width = "60%";
+
+            // let eventImg = document.createElement("img");
+            // let promptToAnswer = document.createElement("span");
+            // promptToAnswer.setAttribute("class", "questionText");
+            // promptToAnswer.innerHTML = "Choose your answer:";
+
+            // eventImg.id = "montyPython";
+            // eventImg.src = "explosion.jpeg";
+
+           // let ans1 = document.createElement("button");
+           // let ans2 = document.createElement("button");
+           // let ans3 = document.createElement("button");
+
+            ans1.setAttribute("class", "answerButton");  //button to progress event
+            ans2.setAttribute("class", "answerButton");  //optional additional buttons
+            ans3.setAttribute("class", "answerButton");
+
+            ans1.innerHTML = "Text in button 1";
+            ans2.innerHTML = "Text in button 2";
+            ans3.innerHTML = "Text in button 3";
+
+            // Append child elements to container div element
+            container.appendChild(eventText);
+            btnContainer.appendChild(ans1);
+            btnContainer.appendChild(ans2);
+            btnContainer.appendChild(ans3);
+            container.appendChild(btnContainer);
+
+            // Append container div element to eventScreen
+            eventScreen.appendChild(container);
+
+            ans1.addEventListener("click", function() {    //below here is everything that happens after first button press
+                let resultText = document.createElement("div");
+                let pickUpItem = document.getElementById("GamePickUpItem");
+                btnContainer.removeChild(ans1);
+                btnContainer.removeChild(ans2);
+                btnContainer.removeChild(ans3);
+
+                // let endTurnBtn = document.getElementById("GameEndTurn");
+                // btnContainer.appendChild(endTurnBtn);
+
+                // endTurnBtn.style.visibility = "visible";
+
+                pickUpItem.style.visibility = "visible";
+                btnContainer.appendChild(pickUpItem);
+
+                eventText.innerHTML = "Followup event text";
+
+                pickUpItem.addEventListener("click", function() {  //below here is everything that happens after the button press
+
+                    //pickUpItem.style.visibility = "hidden";  // optional item adder
+                    eventText.innerHTML = "additional follow up text";
 
                     btnContainer.removeChild(pickUpItem);
                     let endTurnBtn = document.getElementById("GameEndTurn");
