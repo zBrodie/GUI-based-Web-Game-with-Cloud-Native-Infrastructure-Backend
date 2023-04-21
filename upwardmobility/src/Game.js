9bfee8f884346b4ba4926287fc79c653332518cb
Aaron <themahimusic@gmail.com>
2023-04-21 16:10:40 -0400

    pulling

--- upwardmobility/src/Game.js
+++ upwardmobility/src/Game.js
@@ -142,7 +142,8 @@ export const UpwardsMobility = {
 
             const availableEvents = eventsArray.filter(event => event !== G.lastEvent);
             const randomEventIndex = Math.floor(Math.random() * availableEvents.length);
-            G.currentEvent = availableEvents[randomEventIndex];
+            // G.currentEvent = availableEvents[randomEventIndex];
+            G.currentEvent = eventsArray[1]
             console.log("Current event: " + G.currentEvent.id)
             // console.log("Last event: " + G.lastEvent.id)
             G.lastEvent = G.currentEvent;
